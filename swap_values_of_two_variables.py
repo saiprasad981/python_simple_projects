19870449433b7bb196ce48a887af951773d4ce09
sai prasad <vallabhunisaiprasad5@gmail.com>
2025-04-19 00:14:42 +0530

    Create swap_values_of_two_variables.py

---
+++ swap_values_of_two_variables.py
@@ -0,0 +1,7 @@
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+print(f"before swapping:  ,    a value:{a},  b value:{b}")
+temp=a
+a=b
+b=temp
+print(f"After swapping:   ,   a value:{a},  b value:{b}")
