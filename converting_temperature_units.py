eaf2d75cc06b910b9ce9b27a8d6b864ace1001dc
sai prasad <vallabhunisaiprasad5@gmail.com>
2025-04-19 00:17:21 +0530

    Create converting_temperature_units.py

---
+++ converting_temperature_units.py
@@ -0,0 +1,5 @@
+celcius=int(input("Enter Temperature in Celcius:"))
+cel_to_fahrenheit=celcius*(9/5)+32
+cel_to_kelvin=celcius+273
+print(f"In fahrenheit:{cel_to_fahrenheit}")
+print(f"In kelvin:{cel_to_kelvin}")
