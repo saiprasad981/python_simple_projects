c495a3c72a27b4dc797045d21c0bf11432adacf6
sai prasad <vallabhunisaiprasad5@gmail.com>
2025-04-19 22:14:08 +0530

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,7 +1,9 @@
 year=int(input("enter a year:"))
 if year %4 ==0:
     if year % 100==0:
-        if year % 400==0:print("Leap Year")
-        else:print("non leap year")
+        if year % 400==0:
+            print("Leap Year")
+        else:
+            print("non leap year")
     else:print("leap year")
 else:print("non leap year")
